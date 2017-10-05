f67abf2fdd16afa79067ab4263164b2ca33cb9c3
Blueward12 <31855508+Blueward12@users.noreply.github.com>
2017-10-05 10:21:54 -0500

    Add files via upload
    10/5/17 Kevin Ward, Abhi Patel, Ian Williams

---
+++ Program Problem 3/Program Problem 3/Source.cpp
@@ -0,0 +1,52 @@
+/* Kevin Ward, Ian Williams, Abhi Patel - Period 2
+Period Problem 3
+Three Digit Ascending and Descending Selection
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+
+using namespace std;
+
+
+void pause() {
+	cout << "Press any key to continue...";
+	while (!_kbhit());
+	_getch();
+	cout << '\n';
+}
+
+
+void main() {
+	int three_digit_number;
+
+
+	cout << "Enter a three digit number, ";
+	Sleep(2000);
+	cout << "please. : ";
+	cin >> three_digit_number;
+
+
+	int x = three_digit_number;
+	int A = x / 100;
+	int B = (x / 10) % 10;
+	int C = x % 10;
+
+
+	if (A < B && B < C) {
+		cout << "Ascending..." << endl;
+		Sleep(2000);
+	}
+	else if (A > B && B > C) {
+		cout << "Descending..." << endl;
+		Sleep(2000);
+	}
+	else {
+		cout << "Neither..." << endl;
+		Sleep(2000);
+	}
+
+	pause();
+}
