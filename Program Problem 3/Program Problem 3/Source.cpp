336adea70d6336019df06e21dbb16e92dcabdfca
thenicoman <31855428+thenicoman@users.noreply.github.com>
2017-10-16 10:05:59 -0500

    Updated code
    Aidan Nico
    10/16/17
    Code should be faster and is fixed to be more user friendly.

--- Program Problem 3/Program Problem 3/Source.cpp
+++ Program Problem 3/Program Problem 3/Source.cpp
@@ -1,52 +1,54 @@
-+/* Kevin Ward, Ian Williams, Abhi Patel - Period 2
- +Period Problem 3
- +Three Digit Ascending and Descending Selection
- +*/
- +
- +#include <iostream>
- +#include <conio.h>
- +#include <Windows.h>
- +
- +
- +using namespace std;
- +
- +
- +void pause() {
- +	cout << "Press any key to continue...";
- +	while (!_kbhit());
- +	_getch();
- +	cout << '\n';
- +}
- +
- +
- +void main() {
- +	int three_digit_number;
- +
- +
- +	cout << "Enter a three digit number, ";
- +	Sleep(2000);
- +	cout << "please. : ";
- +	cin >> three_digit_number;
- +
- +
- +	int x = three_digit_number;
- +	int A = x / 100;
- +	int B = (x / 10) % 10;
- +	int C = x % 10;
- +
- +
- +	if (A < B && B < C) {
- +		cout << "Ascending..." << endl;
- +		Sleep(2000);
- +	}
- +	else if (A > B && B > C) {
- +		cout << "Descending..." << endl;
- +		Sleep(2000);
- +	}
- +	else {
- +		cout << "Neither..." << endl;
- +		Sleep(2000);
- +	}
- +
- +	pause();
- +} 
+/* Kevin Ward, Ian Williams, Abhi Patel - Period 2
+Period Problem 3
+Three Digit Ascending and Descending Selection
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+
+using namespace std;
+
+//removed kbhit for more madness options
+void pause() {
+
+	cout << '\n';
+}
+
+
+void main() {
+	
+	
+
+		for (char i = 0; i < 30; ++i){
+			int three_digit_number;
+
+
+			//Deleted sleep commands, doesn't make it fast when its using sleep
+			cout << "Would you kindly enter a three digit number, ";
+			cout << "with kindness. : ";
+			cin >> three_digit_number;
+
+
+			int x = three_digit_number;
+			int A = x / 100;
+			int B = (x / 10) % 10;
+			int C = x % 10;
+
+
+				if (A < B && B < C) {
+					cout << "Hey, " << A << B << C << " is pretty good for ascending..." << endl;
+				}
+				else if (A > B && B > C) {
+					cout << "Wow " << A << B << C << " is descending..." << endl;
+				}
+				else {
+					cout << "Feels bad man " << A << B << C << " is neither..." << endl;
+				}
+				
+			}
+	
+		pause();
+
+		}
